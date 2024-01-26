a4eabb7f69f5044a694f815b665cfb5d1e6687aa
Yuri Brunetto <brunettoyuri@gmail.com>
2024-01-26 14:55:54 -0300

    i dunno what im doing lol

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,8 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   CLOUDFLARE_ENDPOINT: z.string().url(),
   CLOUDFLARE_ACCESS_KEY_ID: z.string(),
-  CLOUDFLARE_SECRET_ACCESS_KEY: z.string()
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+  PORT: z.string()
 })
 
 export const env = envSchema.parse(process.env)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,55 +1,101 @@
 import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { PrismaClient } from '@prisma/client'
-import fastify from 'fastify'
-import { z } from 'zod'
-import { r2 } from './../lib/cloudfare'
+import cors from 'cors'
+import express from 'express'
+import formidable from 'formidable'
 import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
+import { env } from './../env'
+import { r2 } from './../lib/cloudflare'
 
-const app = fastify()
+const app = express()
 const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return '🤓'
-})
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '127.0.0.1:3000')
 
-app.post('/uploads', async request => {
-  const uploadBodySchema = z.object({
-    name: z.string().min(1),
-    contentType: z.string()
-  })
-
-  const { name, contentType } = uploadBodySchema.parse(request.body)
-
-  const fileKey = randomUUID().concat('-').concat(name)
-
-  const signedUrl = await getSignedUrl(
-    r2,
-    new PutObjectCommand({
-      Bucket: 'plantinha-dev',
-      Key: fileKey,
-      ContentType: contentType
-    }),
-    { expiresIn: 600 }
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   )
 
-  const file = await prisma.file.create({
-    data: {
-      name,
-      contentType,
-      key: fileKey
-    }
-  })
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
 
-  return { signedUrl, fileId: file.id }
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+  // Pass to next layer of middleware
+  next()
+})
+app.use(express.json({ limit: '50mb' }))
+app.use(
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000000 })
+)
+app.use(cors())
+
+app.get('/', (_, res) => {
+  res.send('🤓')
 })
 
-app.get('/uploads/:id', async request => {
+app.post('/uploads', (req, res, next) => {
+  const form = formidable({})
+
+  form.parse(req, (err, fields, files) => {
+    console.log(files)
+    res.json({ fields, files })
+  })
+  console.log('aaaaa')
+
+  // res.send({ hello: 'world' })
+
+  // const fileKey = randomUUID().concat('-').concat(name)
+  //
+  // const signedUrl = await getSignedUrl(
+  //   r2,
+  //   new PutObjectCommand({
+  //     Bucket: 'plantinha-dev',
+  //     Key: fileKey,
+  //     ContentType: contentType
+  //   }),
+  //   { expiresIn: 600 }
+  // )
+
+  // const newFile = await prisma.file.create({
+  //   data: {
+  //     name,
+  //     contentType,
+  //     key: fileKey
+  //   }
+  // })
+  //
+  // fetch(signedUrl, {
+  //   method: 'PUT',
+  //   headers: {
+  //     'Content-Type': 'image/png'
+  //   },
+  //   body: file
+  // })
+  //   .then(res => {
+  //     console.log('bundaares===>', res)
+  //   })
+  //   .catch(err => {
+  //     console.error(err)
+  //   })
+  //
+  // res.send({ signedUrl, fileId: newFile.id })
+})
+
+app.get('/uploads/:id', async (req, res) => {
   const getFileParamsSchema = z.object({
     id: z.string().cuid()
   })
 
-  const { id } = getFileParamsSchema.parse(request.params)
+  const { id } = getFileParamsSchema.parse(req.params)
 
   const file = await prisma.file.findUniqueOrThrow({
     where: { id }
@@ -64,9 +110,9 @@ app.get('/uploads/:id', async request => {
     { expiresIn: 600 }
   )
 
-  return { signedUrl }
+  res.send({ signedUrl })
 })
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('🔥 HTTP server is running')
+app.listen(env.PORT, () => {
+  console.log(`🚀 Server started at http://localhost:${env.PORT}`)
 })
