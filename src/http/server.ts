3ab96f63d93f935e44feb06d7d00a2e224c2cb54
Yuri Brunetto <brunettoyuri@gmail.com>
2024-03-19 14:05:00 -0300

    remove docker file since it will be handled by the main project

--- src/http/server.ts
+++ src/http/server.ts
@@ -17,26 +17,6 @@ const app = express()
 const prisma = new PrismaClient()
 const upload = multer({ dest: 'uploads/' })
 
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '127.0.0.1:3000')
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  )
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-
-  // Pass to next layer of middleware
-  next()
-})
 app.use(express.json({ limit: '50mb' }))
 app.use(
   express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 2 })
